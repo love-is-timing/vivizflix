ab06d79fefe1efeed149c70df048fef871167ef9
love-is-timing <loveistiming93@gmail.com>
2022-01-16 13:49:00 -0300

    json test

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ function openPopup() {
 function closePopup() {
   document.getElementById("popup").style = "display: none";
 }
+
+fetch("test.json")
+.then(raw => raw.json())
+.then(res => console.log(res))
+.catch(err => console.log(err))
